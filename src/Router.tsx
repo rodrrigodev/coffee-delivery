09db940d52fee63985e95ecc39bb2b95550ca477
rodrrigodev <rodrigosanyy@gmail.com>
2022-11-20 22:16:04 -0300

    navlinks add

--- src/Router.tsx
+++ src/Router.tsx
@@ -9,7 +9,7 @@ export function Router(){
     return(
         <Routes>
             <Route path="/" element={<DefaultLayout/>}>
-            <Route path="/home" element={<Home/>}/>
+            <Route path="/" element={<Home/>}/>
             <Route path="/checkout" element={<Checkout/>}/>
             <Route path="/checkout/filled" element={<CheckoutFilled/>}/>
             <Route path="/checkout-finished/success" element={<Success/>}/>
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+    max-width: 71.25rem;
+    width: 90%;
+    margin: 2rem auto;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`
+
+export const NavActionsContainer = styled.div`
+    display: flex;
+    gap: .75rem;
+    /* align-items: center;
+    justify-content: center; */
+
+    span, a{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        font-size: .875rem;
+        line-height: 1.3;
+        padding: .5rem;
+        border-radius: 6px;
+    }
+
+    span{
+        color: ${props=> props.theme["purple-dark"]};
+        background-color: ${props=> props.theme["purple-light"]};
+    }
+
+    a{
+        background-color: ${props=> props.theme["yellow-light"]};
+    }
+`
+
+export const NavLocation = styled.a
